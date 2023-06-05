692560a401677cb7b0313de28687b106bc4370fe
MiguelMEAC <Meac2001>
2023-06-05 11:22:47 -0500

    creacion del proyecto

--- src/main/java/com/api/invoice/uts/controllers/clienteResController.java
+++ src/main/java/com/api/invoice/uts/controllers/clienteResController.java
@@ -9,6 +9,7 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import com.api.invoice.uts.services.IclienteServices;
 
 import jakarta.validation.Valid;
 
+@CrossOrigin(origins= {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api")
 public class clienteResController {
